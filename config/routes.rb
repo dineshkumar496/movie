5f703a60eacf7b5dd881a94c62b9fcb4d3e0f404
dineshkumar D <dineshkumar.dharmar@mallow-tech.com>
2023-04-21 09:49:41 +0530

    added devise and configured controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   get 'test/index'
   root 'test#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
